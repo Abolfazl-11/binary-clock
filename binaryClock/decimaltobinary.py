1befdaf0d807b36ebed0fc7a9e20d9241f1b620c
Near Huscarl <near.huscarl@gmail.com>
2022-05-29 09:27:15 +0430

    modified decimaltobinary.py

--- binaryClock/decimaltobinary.py
+++ binaryClock/decimaltobinary.py
@@ -1,14 +1,13 @@
-def dectobin(n, d):
-     num = n
-     b = [0 for i in range(d)]  
-     t = [1024, 512, 256, 128, 64 ,32, 16, 8, 4, 2, 1]
-     t = t[len(t) - d:]
-     for i in range(d):
-         v = num // t[i]                          
-         b[i] = str(v)                            
-         num -= t[i] * v                          
-     return "".join(b)
+def dectobin(n:int, d:int):
+    num = n
+    b = [0 for i in range(d)]
+    for i in range(d, 0, -1):
+        a = num // (2**(i - 1))                          
+        b[d - i] = str(a)                            
+        num -= (2**(i - 1)) * a                          
+    return ''.join(b)
 
 if __name__ == "__main__":
     num, digit = [int(i) for i in input().split()]
-    print(binary(num, digit))
+    r = dectobin(num, digit)
+    print(r)
